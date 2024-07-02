4a52257d20217d2f647b1223903737b90f46313f
Your Name <you@example.com>
2024-07-02 14:24:27 +0100

    further name change

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,7 +12,7 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Debs2"));
+        System.out.println(sayHelloToSomeone("Debs3"));
         System.out.println(sayGoodbye());
     }
 
